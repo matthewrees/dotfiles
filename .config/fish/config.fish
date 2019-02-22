bff2bde197d25b8373c6c84802f61148e9c08a0b
Matthew Rees <matthew@teamrees.ca>
2019-02-22 08:21:43 -0800

    Hide gpg agent startup and use libvirt for Vagrant

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpg-connect-agent updatestartuptty /bye >/dev/null
+gpg-connect-agent updatestartuptty /bye > /dev/null 2>&1
+
+set -x VAGRANT_DEFAULT_PROVIDER libvirt
