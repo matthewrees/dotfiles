656d3ca52d7c40a495fc2302ae173055d9f9da87
Matthew Rees <matthew@teamrees.ca>
2019-04-11 14:07:44 -0700

    Add comments and include home local bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
+# settings for yubikey
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 gpg-connect-agent updatestartuptty /bye > /dev/null 2>&1
 
-set -x VAGRANT_DEFAULT_PROVIDER libvirt
+# include home bin path
+set -x PATH $PATH ~/.local/bin
