55a3642ee93cd227faba68292303d656152f7ca4
Matthew Rees <matthew@teamrees.ca>
2019-08-01 15:38:26 -0400

    Cleaned up linking a bit

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-for file in $(ls -a ~/git/dotfiles/ | tail -n +3 | grep -v README.md | grep -v install.sh | grep -v .git$ | grep -v .gitignore)
+DOTFILES_ROOT=$(pwd -P)
+
+for file in $(ls -a $DOTFILES_ROOT | tail -n +3 | grep -v README.md | grep -v install.sh | grep -v .git$ | grep -v .gitignore | grep -v install.brew)
 do
-  if [[ ! -e "$HOME/$file" ]]; then
-    ln -s "$HOME/git/dotfiles/$file" "$HOME/$file"
-  fi
-  chmod 700 .gnupg
+	if [[ ! -e "$HOME/$file" ]]
+	then
+		ln -s "$HOME/git/dotfiles/$file" "$HOME/$file"
+	else
+		echo "Could not link $HOME/git/dotfiles/$file as it already exists"
+	fi
+
+	chmod 700 .gnupg
 done
