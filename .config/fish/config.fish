4ffefb2af3619c6f50a4dad9d22d9e627e435eb7
Matthew Rees <matthew@teamrees.ca>
2019-08-01 15:37:44 -0400

    Working gpg-agent on laptop

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 # settings for yubikey
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpg-connect-agent updatestartuptty /bye > /dev/null 2>&1
+gpgconf --launch gpg-agent
 
 # include home bin path
 set -x PATH $PATH ~/.local/bin
+
