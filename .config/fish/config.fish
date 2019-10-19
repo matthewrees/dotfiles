d4c35518c0fcd8bcad092413c1879fbfa05e1cc8
Matthew Rees <matthew@teamrees.ca>
2019-10-18 21:27:11 -0400

    Swap fish for zsh

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-# settings for yubikey
-set -x GPG_TTY (tty)
-set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
-
-# include home bin path
-set -x PATH $PATH ~/.local/bin
-
