3f3b54019a83ec962815308059e632825703c576
Matthew Rees <matthew@teamrees.ca>
2020-05-21 13:58:36 -0400

    Ignore brew listings

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DOTFILES_ROOT=$(pwd -P)
 
-for file in $(ls -a $DOTFILES_ROOT | tail -n +3 | grep -v README.md | grep -v install.sh | grep -v .git$ | grep -v .gitignore | grep -v install.brew)
+for file in $(ls -a $DOTFILES_ROOT | tail -n +3 | grep -v README.md | grep -v install.sh | grep -v .git$ | grep -v .gitignore | grep -v "brew-")
 do
 	if [[ ! -e "$HOME/$file" ]]
 	then
