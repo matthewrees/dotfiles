7223e306b495691629ec20549df289ad3d676353
Matthew Rees <matthew@teamrees.ca>
2020-09-05 13:57:46 -0400

    Add AWS CLI and remove echo

--- homebrew.sh
+++ homebrew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PACKAGES=("neovim --HEAD" "zig --HEAD" "starship")
+PACKAGES=("neovim --HEAD" "zig --HEAD" "starship" "awscli")
 CASKS=("font-fira-code-nerd-font" "rectangle" "authy")
 TAPS=("homebrew/cask-fonts" "filippo.io/yubikey-agent")
 SERVICES=("yubikey-agent")
@@ -12,26 +12,26 @@ INSTALLED_SERVICES=$(brew services)
 
 for tap in "${TAPS[@]}"; do
 	if [[ ${INSTALLED_TAPS} != *"$tap"* ]]; then
-		echo brew tap $tap
+		brew tap $tap
 	fi
 done
 
 for cask in "${CASKS[@]}"; do
 	if [[ ${INSTALLED_CASKS} != *"$cask"* ]]; then
-		echo brew cask install $cask
+		brew cask install $cask
 	fi
 done
 
 for package in "${PACKAGES[@]}"; do
 	temp=$(echo $package | sed 's/--//')
 	if [[ ${INSTALLED_PACKAGES} != *"$temp"* ]]; then
-		echo brew install $package
+		brew install $package
 	fi
 done
 
 for service in "${SERVICES[@]}"; do
 	if [[ ${INSTALLED_SERVICES} != *"$service"* ]]; then
-		echo brew services start $service
+		brew services start $service
 	fi
 done
 
