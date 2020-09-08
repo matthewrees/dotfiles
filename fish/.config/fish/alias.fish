8ad837ccb940f78f40e3ccbc4ee71fffab4e4915
Matthew Rees <mrees@hey.com>
2020-09-07 21:05:04 -0400

    Add aliases

---
+++ fish/.config/fish/alias.fish
@@ -0,0 +1,19 @@
+alias vim=nvim
+alias vi=nvim
+alias ls=exa
+
+alias l='ls -l'
+alias ll='ls -la'
+alias lt='ls --tree'
+
+alias reload='exec fish'
+alias of='open .'
+
+abbr -a gs git status -sbu
+abbr -a ga git add
+abbr -a gco git checkout
+abbr -a gcl git clone
+abbr -a gd git diff
+abbr -a gp git remote prune origin
+abbr -a gcm git commit -m
+abbr -a sshr ssh-keygen -f ~/.ssh/known_hosts -R
